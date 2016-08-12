4690a7a4f59397af54432e3b762cf5be4f28f10b
rsfarkas <roxannesfarkas@gmail.com>
2016-08-11 23:39:11 -0400

    added untracked files

---
+++ JS/js/main.js
@@ -0,0 +1,38 @@
+// // this is how you comment in JS
+
+// var myVariable = 10;
+
+// myFunction();
+
+// "This is a quote from me: \" Hi. My name is cat. \""
+// -escape the characters so the computer doest see 
+// the first escaped quote and end the string.
+
+//First Demo Alert
+alert ("If you are worried that your js file isnt link properly");
+
+var mynumber;
+var myOtherNumber;
+var liar;
+var pizza_hut;
+
+mynumber = 10;
+myOtherNumber = 1000.10;
+liar = false;
+pizza_hut = 9;
+
+var myName = "Roxy";
+
+console.log("I have ", mynumber, " in my bank account.");
+console.log("My name is: ",myName);
+
+// document.getElementById("radish");
+// document.getElementClassName('main_container');
+// document.onLoad(){
+// 	do something when the document loads
+// }
+
+document.getElementById("main_paragraph").style.color="pink";
+
+docunet.getElementByClassName("main_container"[0].style.color="blue")
+
